bf7ee3f615e7791768888c1437f85462ddf981ae
YunBok <qodbsqhr@naver.com>
2022-01-20 23:27:21 +0900

    mustache를 이용한 첫화면 띄우기 및 테스트케이스 작성

--- src/main/java/com/yunbok/admin/springboot/Application.java
+++ src/main/java/com/yunbok/admin/springboot/Application.java
@@ -3,11 +3,13 @@ package com.yunbok.admin.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.swing.*;
 
 @EnableJpaRepositories(basePackages="com.yunbok.admin.springboot.domain")
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 

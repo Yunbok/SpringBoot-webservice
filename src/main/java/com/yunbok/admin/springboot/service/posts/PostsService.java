40c931868f5b6d2ae0387758c5b90ec3c6019a14
Yunbok <qodbsqhr@naver.com>
2022-01-23 00:13:21 +0900

    수정 삭제기능 커밋

--- src/main/java/com/yunbok/admin/springboot/service/posts/PostsService.java
+++ src/main/java/com/yunbok/admin/springboot/service/posts/PostsService.java
@@ -3,13 +3,16 @@ package com.yunbok.admin.springboot.service.posts;
 
 import com.yunbok.admin.springboot.domain.posts.Posts;
 import com.yunbok.admin.springboot.domain.posts.PostsRepository;
+import com.yunbok.admin.springboot.web.dto.PostsListResponseDto;
 import com.yunbok.admin.springboot.web.dto.PostsResponseDto;
 import com.yunbok.admin.springboot.web.dto.PostsSaveRequestDto;
 import com.yunbok.admin.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -38,4 +41,20 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow( () -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
+
+        postsRepository.delete(posts);
+    }
+
 }
---
+++ src/main/java/com/yunbok/admin/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,20 @@
+package com.yunbok.admin.springboot.web.dto;
+
+import com.yunbok.admin.springboot.domain.posts.Posts;
+
+import java.time.LocalDateTime;
+
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}

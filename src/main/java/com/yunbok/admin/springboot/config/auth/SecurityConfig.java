c16131a44bc1909b92c13e7767aa3990a9c6bd6b
YunBok <qodbsqhr@naver.com>
2022-01-25 00:26:24 +0900

    스프링 시큐리티 구글 OAuth 적용

---
+++ src/main/java/com/yunbok/admin/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.yunbok.admin.springboot.config.auth;
+
+
+import com.yunbok.admin.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2userService
+            customOAuth2UserService;
+
+    @Override
+    protected  void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/","/css/**","/images/**"
+                        ,"/js/**" , "/h2-console/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+
+    }
+}

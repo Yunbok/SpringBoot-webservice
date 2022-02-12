a4e5dde48794d261df42865546e97a15084d8623
Yunbok <qodbsqhr@naver.com>
2022-02-12 22:46:14 +0900

    테스트 하나 실패 405오류

--- src/test/java/com/yunbok/admin/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/yunbok/admin/springboot/web/PostsApiControllerTest.java
@@ -84,8 +84,8 @@ public class PostsApiControllerTest {
 
 
         //then
-//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+//      assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//      assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
